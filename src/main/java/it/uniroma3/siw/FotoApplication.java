4562ff2233c33b86d1818c8a762a37365fc3de0d
Lucass97 <luca.gregori97@gmail.com>
2019-06-05 14:22:47 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/FotoApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoApplication.class, args);
+	}
+
+}
