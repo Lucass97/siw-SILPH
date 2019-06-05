eb3d407c57dea8061443199ce80cc10bc8be5b2c
Cristian Rotaru <cristianrotaru41@gmail.com>
2019-06-05 16:07:49 +0200

    richiesta merge
    creazione Fotografo.java & FotografoRepository

---
+++ src/main/java/it/uniroma3/siw/model/Fotografo.java
@@ -0,0 +1,89 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fotografo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	
+	private String cognome;
+	
+	private LocalDate dataNascita;
+	
+	private String email;
+
+	/* METODI GETTERS & SETTERS */
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public LocalDate getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(LocalDate dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
+	/* METODI DI SERVIZIO */
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fotografo other = (Fotografo) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ src/main/java/it/uniroma3/siw/repository/FotografoRepository.java
@@ -0,0 +1,10 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import it.uniroma3.siw.model.Fotografo;
+
+public interface FotografoRepository  extends CrudRepository<Fotografo, Long>{
+
+}
