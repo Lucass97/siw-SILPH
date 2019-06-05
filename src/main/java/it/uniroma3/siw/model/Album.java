f7c7dc977d675e27962fb4cb9f0487dea575d70f
Lucass97 <luca.gregori97@gmail.com>
2019-06-05 15:20:35 +0200

    primo modifiche sul modello di dominio

---
+++ src/main/java/it/uniroma3/siw/model/Album.java
@@ -0,0 +1,82 @@
+package it.uniroma3.siw.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Album {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String titolo;
+	
+	@OneToMany(cascade = {CascadeType.PERSIST})
+	private List<Foto> foto;
+
+	/* METODI GETTERS & SETTERS */
+	
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public List<Foto> getFoto() {
+		return foto;
+	}
+
+	public void setFoto(List<Foto> foto) {
+		this.foto = foto;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
+	/* METODI DI SERVIZIO */
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Album other = (Album) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Album [id=" + id + ", titolo=" + titolo + "]";
+	}
+	
+	/* COSTRUTTORI */
+	
+	public Album (){
+		this.foto = new LinkedList<Foto>();
+	}
+	
+	
+}
---
+++ src/main/java/it/uniroma3/siw/repository/AlbumRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Album;
+
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+	
+}
