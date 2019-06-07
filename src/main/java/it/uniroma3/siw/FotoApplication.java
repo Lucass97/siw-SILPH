840aa8adb3b360b6ebe8d50d20a153570ca3bb70
Lucass97 <luca.gregori97@gmail.com>
2019-06-07 12:46:00 +0200

    altre aggiunte per il layer storage

--- src/main/java/it/uniroma3/siw/FotoApplication.java
+++ src/main/java/it/uniroma3/siw/FotoApplication.java
@@ -1,13 +1,28 @@
 package it.uniroma3.siw;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import it.uniroma3.siw.controller.storage.StorageProperties;
+import it.uniroma3.siw.controller.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FotoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FotoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
