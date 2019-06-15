515469c834c2f81ffa921901163aecbd7ed9d3fb
Lucass97 <luca.gregori97@gmail.com>
2019-06-15 23:49:15 +0200

    gestione login

--- src/main/java/it/uniroma3/siw/controller/MainController.java
+++ src/main/java/it/uniroma3/siw/controller/MainController.java
@@ -8,31 +8,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.uniroma3.siw.model.Funzionario;
+import it.uniroma3.siw.services.FotoService;
 
 @Controller
 public class MainController {
-	
-	 @Autowired
-	    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  // Login form
-  @GetMapping("/login")
-  public String login(Model model) {
-	model.addAttribute("funzionario",new Funzionario());
-	System.out.println(bCryptPasswordEncoder.encode("ciao1234"));
-    return "login.html";
-  }
+	@Autowired
+	private FotoService fotoService;
 
-  // Login form with error
-  @RequestMapping("/login-error.html")
-  public String loginError(Model model) {
-    model.addAttribute("loginError", true);
-    return "login.html";
-  }
-  
-  @GetMapping("/statoLogin")
-  public String statologin() {
-    return "successoLogin.html";
-  }
+	// Login form
+	@GetMapping("/")
+	public String login(Model model) {
+		model.addAttribute("funzionario",new Funzionario());
+		model.addAttribute("fotos",this.fotoService.getRandomFoto(5));
+		return "index.html";
+	}
+
+	// Login form with error
+	@RequestMapping("/login-error.html")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login.html";
+	}
+
+	@GetMapping("/statoLogin")
+	public String statologin() {
+		return "successoLogin.html";
+	}
 
 }
