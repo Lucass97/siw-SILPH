8c25be09ec7e771fa4a4c6d87cdd006be78088a9
Lucass97 <luca.gregori97@gmail.com>
2019-06-15 23:51:35 +0200

    aggiornati repository
    cambiato da CrudRepository a JpaRepository. Aggiunte anche delle query personalizzate per la ricerca

--- src/main/java/it/uniroma3/siw/repository/AlbumRepository.java
+++ src/main/java/it/uniroma3/siw/repository/AlbumRepository.java
@@ -2,13 +2,18 @@ package it.uniroma3.siw.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import it.uniroma3.siw.model.Album;
 
 @Repository
-public interface AlbumRepository extends CrudRepository<Album, Long>{
+public interface AlbumRepository extends JpaRepository<Album, Long>{
 	
-	List<Album> findByTitolo(String titolo);
+	@Query(value = "SELECT * FROM Album ORDER BY RANDOM() LIMIT ?1", nativeQuery = true)
+	List<Album> getRandomAlbum(int limit);
+	
+	@Query(value = "SELECT a FROM Album a WHERE a.titolo LIKE %?1% OR a.descrizione LIKE %?1% ORDER BY a.titolo")
+	List<Album> ricercaAlbumByParametro(String parametro);
 }
--- src/main/java/it/uniroma3/siw/repository/FotoRepository.java
+++ src/main/java/it/uniroma3/siw/repository/FotoRepository.java
@@ -2,12 +2,12 @@ package it.uniroma3.siw.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.model.Foto;
 
-public interface FotoRepository  extends CrudRepository<Foto, String>{
+public interface FotoRepository  extends JpaRepository<Foto, String>{
 
 	@Query(value = "SELECT * FROM Foto ORDER BY RANDOM() LIMIT ?1", nativeQuery = true)
 	List<Foto> getRandomFoto(int limit);
