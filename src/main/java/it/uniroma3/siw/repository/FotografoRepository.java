51c7d006b1028bf07865f3a2096690afc1f8030f
Lucass97 <luca.gregori97@gmail.com>
2019-06-17 21:41:23 +0200

    tolto %

--- src/main/java/it/uniroma3/siw/repository/FotografoRepository.java
+++ src/main/java/it/uniroma3/siw/repository/FotografoRepository.java
@@ -12,6 +12,6 @@ public interface FotografoRepository  extends CrudRepository<Fotografo, Long>{
 	@Query(value = "SELECT * FROM Fotografo ORDER BY RANDOM() LIMIT ?1", nativeQuery = true)
 	List<Fotografo> getRandomFotografi(int limit);
 	
-	@Query(value = "SELECT f FROM Fotografo f WHERE f.nome LIKE %?1% OR f.cognome LIKE %?1% OR f.email LIKE %?1% ORDER BY f.nome")
+	@Query(value = "SELECT f FROM Fotografo f WHERE f.nome LIKE ?1 OR f.cognome LIKE ?1 OR f.email LIKE ?1 ORDER BY f.nome")
 	List<Fotografo> findFotografiByParametro(String parametro);
 }
